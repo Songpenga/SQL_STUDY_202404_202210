3f0d2daed8c2bb49d2973a9de914e87ee0f35ca2
Songpenga <iiuuyy6654@gmail.com>
2024-05-22 11:08:17 +0900

    Create INSERT_여러개_240522.sql

---
+++ INSERT_여러개_240522.sql
@@ -0,0 +1,6 @@
+INSERT ALL INTO 다중 (AA, BB, CC, USE, EE) VALUES ('0', '1', '2', '3', '4')
+           INTO 다중 (AA, BB, CC, USE, EE) VALUES ('0', '1', '2', '3', '4')
+           INTO 다중 (AA, BB, CC, USE, EE) VALUES ('0', '1', '2', '3', '4')
+           INTO 다중 (AA, BB, CC, USE, EE) VALUES ('0', '1', '2', '3', '4')
+           INTO 다중 (AA, BB, CC, USE, EE) VALUES ('0', '2', '2', '3', '4')
+          SELECT * FROM DUAL;
