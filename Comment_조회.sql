61347f61f2aba418a6c39af5c9d5242e4a7163f9
Songpenga <116074401+Songpenga@users.noreply.github.com>
2025-01-04 23:05:37 +0900

    (수정) Update Comment_조회

--- Comment_조회.sql
+++ Comment_조회.sql
@@ -1,3 +1,5 @@
-SELECT *
-  FROM ALL_COL_COMMENTS
- WHERE TABLE_NAME = [���̺��̸�];
+-- 특정 테이블 코멘트 조회
+select * from ALL_TAB_COMMENTS WHERE TABLE_NAME='테이블 이름';
+
+-- 특정 테이블 컬럼 코멘트 조회
+SELECT * FROM ALL_COL_COMMENTS WHERE TABLE_NAME='테이블 이름';
